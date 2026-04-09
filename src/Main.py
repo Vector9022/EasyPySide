2b51e94a86e90d906fc11a8f5cefce961aa4c6a6
vector9022 <artem90321@gmail.com>
2026-04-09 07:47:33 +0300

    v2.0

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout
 from localLib.EasyPySide import BaseWindow
+from localLib.EasyPySide.Widgets.UniversalMediaContainer import UniversalMediaContainer
 
 class Window(BaseWindow):
     def __init__(self):
@@ -11,8 +12,11 @@ class Window(BaseWindow):
 
         content = QVBoxLayout(self.contentArea)
         content.setContentsMargins(10, 10, 10, 10)
-        content.addWidget(QLabel("Контент окна"))
-        content.addWidget(QPushButton("Кнопка"))
+
+        widget = UniversalMediaContainer()
+        widget.SetContent(r"DevRes\jpg2.jpg")
+
+        content.addWidget(widget)
 
 def Main():
     app = QApplication(sys.argv)
